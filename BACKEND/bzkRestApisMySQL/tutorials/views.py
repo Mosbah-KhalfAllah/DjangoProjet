c7b74a8b8ccc5d9e8e80566e21893fbb00352e7c
Khalfallah Mosbah <kalfallahmosbah71@gmail.com>
2024-12-08 14:33:53 +0100

    Finale code

--- BACKEND/bzkRestApisMySQL/tutorials/views.py
+++ BACKEND/bzkRestApisMySQL/tutorials/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
-from tutorials.models import *
 from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser 
+from rest_framework.parsers import JSONParser
 from rest_framework import status
- 
+
 from tutorials.models import Tutorial
 from tutorials.serializers import TutorialSerializer
 from rest_framework.decorators import api_view
+
+
 @api_view(['GET', 'POST', 'DELETE'])
 def tutorial_list(request):
     # GET list of tutorials, POST a new tutorial, DELETE all tutorials
-     if request.method == 'GET':
+    if request.method == 'GET':
         tutorials = Tutorial.objects.all()
         
         title = request.GET.get('title', None)
@@ -19,4 +20,18 @@ def tutorial_list(request):
         
         tutorials_serializer = TutorialSerializer(tutorials, many=True)
         return JsonResponse(tutorials_serializer.data, safe=False)
-        
+
+    elif request.method == 'POST':
+        tutorial_data = JSONParser().parse(request)
+        tutorial_serializer = TutorialSerializer(data=tutorial_data)
+        if tutorial_serializer.is_valid():
+            tutorial_serializer.save()
+            return JsonResponse(tutorial_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        count = Tutorial.objects.all().delete()
+        return JsonResponse(
+            {'message': f'{count[0]} Tutorials were deleted successfully!'},
+            status=status.HTTP_204_NO_CONTENT
+        )
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DjangoProjet
+Mosbah Khalfallah
